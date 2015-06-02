49a14340e15524abc7c2ffc74957dbc637e9546e
Greg <greg@blacksintechnology.net>
2015-06-01 23:18:07 -0400

    adding waxpoetic.py program

---
+++ waxpoetic.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+import random
+nouns = ['cow', 'turtle', 'horse', 'rabbit', 'unicorn', 'gargoyle', 'mountain']
+verbs = ['kicks', 'jingles', 'bounces', 'slurps', 'meows', 'explodes', 'curdles']
+adjectives = ['furry', 'balding', 'incredulous', 'fragrant', 'exuberant', 'glistening']
+prepositions = ['against', 'after', 'into', 'beneath', 'upon', 'for', 'in', 'like', 'over', 'within']
+adverbs = ['curiously', 'extravagantly', 'tantalizingly', 'furiously', 'sensuously']
+
+noun1 = random.choice(nouns)
+print noun1
