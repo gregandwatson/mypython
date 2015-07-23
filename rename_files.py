903240b1a01ea116984b27243e6cb3ede68ec059
Greg <greg@blacksintechnology.net>
2015-07-23 00:31:42 -0400

    using import glob to search directories

--- rename_files.py
+++ rename_files.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python
 import os
+import glob
 
-my_path ="/home/ggreenlee" 
-file_names_list = os.listdir(my_path)
-for file_name in file_names_list:
-	if file_name.lower().endswith(".jpg"):
-		print 'Converting "{}" to GIF...'.format(file_name)
-		full_file_name = os.path.join(my_path, file_name)
-		new_file_name = full_file_name[0:len(full_file_name)-4] +".gif"
-		os.rename(full_file_name, new_file_name)
+my_path = "/root" 
+possible_files = os.path.join(my_path, "*.gif")
+print possible_files
+print glob.glob(possible_files)
+for file_name in glob.glob(possible_files):
+	print file_name
+	print 'Converting "{0}" to JPG...'.format(file_name)
+	full_file_name = os.path.join(my_path, file_name)
+	print full_file_name
+	new_file_name = full_file_name[0:len(full_file_name)-4] +".jpg"
+	os.rename(full_file_name, new_file_name)
 #print file_names_list
