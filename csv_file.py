43ab5a9e5119ca54c2c5eda183b9b77ce100375a
Greg <greg@blacksintechnology.net>
2015-08-24 23:44:23 -0400

    learning how to use csv module

--- csv_file.py
+++ csv_file.py
@@ -3,7 +3,7 @@
 import os
 import csv
 
-my_path = "/root/chapter10"
+my_path = "/root/chapter10/book1-exercises/Course materials/Chapter 10/Practice files"
 my_file = open(os.path.join(my_path, "wonka.csv"), "rb")
 
 my_file_reader = csv.reader(my_file)
